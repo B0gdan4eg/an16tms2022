0ca8f3ab4bb9ecc0ab4cafe99d62041a0daa552b
B0gdan4eg <shcherbakov.b@mail.ru>
2022-05-04 19:54:23 +0300

    Решение домашнего задания

---
+++ Lesson.1/src/main/java/Task2.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int count = 0;
+        String str = in.nextLine();
+        int[] numArr = Arrays.stream(str.split(" ")).mapToInt(Integer::parseInt).toArray();
+        for (int i : numArr) {
+            if (i >= 0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
