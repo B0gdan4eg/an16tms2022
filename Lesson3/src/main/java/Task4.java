c09e05936938c7fbca66554e645034a8be7e8c97
B0gdan4eg <shcherbakov.b@mail.ru>
2022-05-12 19:08:10 +0300

    Исправления

--- Lesson3/src/main/java/Task4.java
+++ Lesson3/src/main/java/Task4.java
@@ -3,7 +3,7 @@ import java.util.Random;
 public class Task4 {
     public static void main(String[] args) {
         Random random = new Random();
-        int time = random.nextInt(28800);
+        int time = random.nextInt(28801);
         int countOfHours = time / 3600;
         if (countOfHours != 0) {
             if (countOfHours > 1) {
