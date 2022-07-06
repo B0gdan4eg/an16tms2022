4c25b6aa04ffc559171af00fa55b585377f89446
B0gdan4eg <shcherbakov.b@mail.ru>
2022-07-06 20:15:15 +0300

    Build

---
+++ src/main/java/by/shcherbakov/net/javacode/country/Country.java
@@ -0,0 +1,28 @@
+package by.shcherbakov.net.javacode.country;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@Entity
+public class Country {
+    private Long id;
+    private String country;
+    private String description;
+
+
+    protected Country() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long getId() {
+        return id;
+    }
+}
